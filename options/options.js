209672264dc2de875634e733b6283dba1f169519
Crowdbotics <team@crowdbotics.com>
2023-12-16 19:46:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-tooth-45054",
+  url: "https://cool-tooth-45054.botics.co",
+  api: "https://cool-tooth-45054.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
